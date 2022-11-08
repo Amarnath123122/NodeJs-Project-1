0b6caa72a3ab0c35ecfa77a14b1a32ca2adb3e8e
Amarnath.R <Amarnath.R@LMMUM-LP-0551>
2022-11-08 18:04:29 +0530

    Basic Authentication Project

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-/* eslint-disable prettier/prettier */
 const mongooose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err =>{
+  console.log('Uncaught Exception');
+  console.log(err.name, err.message);
+  process.exit(1)
+})
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
@@ -8,20 +13,8 @@ const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
-//const LOCALDB = process.env.DATABASE_LOCAL;
-mongooose.Schema({
-  name:{
-    type:String,
-    required: [true, "the tour name is required"]
-  },
-  name:{
-    type:String,
-    required: [true, "the tour name is required"]
-  },
 
-});
 mongooose
-  // eslint-disable-next-line no-undef
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -31,6 +24,15 @@ mongooose
     console.log('DB CONNECTED');
   });
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection', err =>{
+  console.log('Unhandled Rejection');
+  console.log(err.name, err.message);
+  server.close(() => {
+    console.log("server closed");
+    process.exit(1);
+  });
+})
